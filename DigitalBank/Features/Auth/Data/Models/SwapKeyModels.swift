ad3ef9c08204c00c6f29b5a65b22370d6ec14e9a
Muhammad <muhammad.tohirov@fido.uz>
2025-08-08 15:08:25 +0500

    feat(auth): add SwapKeyModels Request and Response

---
+++ DigitalBank/Features/Auth/Data/Models/SwapKeyModels.swift
@@ -0,0 +1,23 @@
+//
+//  SwapKeyModels.swift
+//  DigitalBank
+//
+//  Created by Muhammad on 08/08/25.
+//
+
+enum SwapKeyModels {
+    struct Request: Equatable {
+        let public_key1: String // g
+        let public_key2: String // p
+        let encryptData: String // big_A
+        let device_code: String
+        let isPasEncrypt: Int
+        let phoneNumber: String?
+    }
+    
+    struct Response: Equatable {
+        var code: Int?
+        var msg: String?
+        var enecnryptData: String?
+    }
+}
