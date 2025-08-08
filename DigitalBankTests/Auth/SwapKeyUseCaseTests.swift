024fa004a70bb320581c9a872bc7f250dec488e6
Muhammad <muhammad.tohirov@fido.uz>
2025-08-08 14:38:54 +0500

    test(auth): create empty SwapKeyUseCaseTests file

---
+++ DigitalBankTests/Auth/SwapKeyUseCaseTests.swift
@@ -0,0 +1,13 @@
+//
+//  SwapKeyUseCaseTests.swift
+//  DigitalBankTests
+//
+//  Created by Muhammad on 08/08/25.
+//
+
+import XCTest
+@testable import DigitalBank
+
+final class SwapKeyUseCaseTests: XCTestCase {
+    
+}
