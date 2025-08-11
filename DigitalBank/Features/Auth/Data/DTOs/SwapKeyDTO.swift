d46187cd787fb9b1c4d2483672b29b9f1cb11b58
Muhammad <muhammad.tohirov@fido.uz>
2025-08-11 12:14:18 +0500

    feat(auth): add DTO and Mapper for SwapKey Response

---
+++ DigitalBank/Features/Auth/Data/DTOs/SwapKeyDTO.swift
@@ -0,0 +1,18 @@
+//
+//  SwapKeyDTO.swift
+//  DigitalBank
+//
+//  Created by Muhammad on 11/08/25.
+//
+
+enum SwapKeyDTO {
+    struct Response: Decodable {
+        let code: Int?
+        let msg: String?
+        let ecnryptData: String?
+
+        func toDomain() -> SwapKeyModels.Response {
+            .init(code: code, msg: msg, ecnryptData: ecnryptData)
+        }
+    }
+}
