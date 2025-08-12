a8a4ebfb798102f2f53494c5ceeba8353a32cc88
Muhammad <muhammad.tohirov@fido.uz>
2025-08-12 09:53:22 +0500

    test(auth): init of GetIPUseCase does not request IP from repository

--- DigitalBankTests/Auth/GetIPUseCaseTests/GetIPUseCaseTests.swift
+++ DigitalBankTests/Auth/GetIPUseCaseTests/GetIPUseCaseTests.swift
@@ -9,5 +9,15 @@ import XCTest
 @testable import DigitalBank
 
 class GetIPUseCaseTests: XCTestCase {
-    
+    // What + When + From/Where
+    func test_init_doesNotRequestIPFromRepository() {
+        // MARK: - Given
+        let repo = GetIPRepositorySpy()
+        
+        // MARK: - When
+        _ = GetIPUseCaseImpl(repository: repo)
+        
+        //MARK: - Then
+        XCTAssertTrue(repo.receivedRequests.isEmpty)
+    }
 }
