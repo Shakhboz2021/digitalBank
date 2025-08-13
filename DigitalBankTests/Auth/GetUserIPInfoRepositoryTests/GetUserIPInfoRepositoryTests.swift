88ae39bf001b05db465dade9e7dc5964972cd6fe
Muhammad <muhammad.tohirov@fido.uz>
2025-08-13 11:11:09 +0500

    test(auth): repository propogates client error in getUserIPInfo

--- DigitalBankTests/Auth/GetUserIPInfoRepositoryTests/GetUserIPInfoRepositoryTests.swift
+++ DigitalBankTests/Auth/GetUserIPInfoRepositoryTests/GetUserIPInfoRepositoryTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+
 @testable import DigitalBank
 
 final class GetUserIPInfoRepositoryTests: XCTestCase {
@@ -16,5 +17,21 @@ final class GetUserIPInfoRepositoryTests: XCTestCase {
         _ = GetUserIPInfoRepositoryImpl(client: client)
         // Then
         XCTAssertTrue(client.receivedRequests.isEmpty)
+    }
+
+    func test_execute_propogateClientError() async throws {
+        // Given
+        let client = GetUserIPInfoClientSpy()
+        let sut = GetUserIPInfoRepositoryImpl(client: client)
+        let expectedError = NSError(domain: "Test", code: 1)
+        client.resultToReturn = .failure(expectedError)
+        //When & Then
+        do {
+            _ = try await sut.getUserIPInfo()
+            XCTFail("Expected to fail but successed")
+        } catch {
+            XCTAssertEqual((error as NSError).domain, expectedError.domain)
+            XCTAssertEqual((error as NSError).code, expectedError.code)
+        }
     }
 }
