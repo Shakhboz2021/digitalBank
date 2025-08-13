c5bb12713dd5192509d2a55351ae458ee2ec5166
Muhammad <muhammad.tohirov@fido.uz>
2025-08-13 10:19:41 +0500

    test(auth) create empty GetUserIPInfoRepositoryTests file

---
+++ DigitalBankTests/Auth/GetUserIPInfoUseCaseTests/GetUserIPInfoRepositoryTests.swift
@@ -0,0 +1,13 @@
+//
+//  GetUserIPInfoRepositoryTests.swift
+//  DigitalBankTests
+//
+//  Created by Muhammad on 13/08/25.
+//
+
+import XCTest
+@testable import DigitalBank
+
+final class GetUserIPInfoRepositoryTests: XCTestCase {
+    
+}
