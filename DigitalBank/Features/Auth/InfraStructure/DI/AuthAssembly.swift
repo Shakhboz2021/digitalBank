c59f1cb5bcdbc156809ae59e22a1c8429cb25ab2
Muhammad <muhammad.tohirov@fido.uz>
2025-08-13 12:34:39 +0500

    chore(auth-di): wire GetUserIPInfo chain (Client -> Repository -> UseCase) in Assembly

--- DigitalBank/Features/Auth/InfraStructure/DI/AuthAssembly.swift
+++ DigitalBank/Features/Auth/InfraStructure/DI/AuthAssembly.swift
@@ -9,12 +9,19 @@ import Foundation
 
 protocol AuthAssembling {
     func makeSwapKeyUseCase() -> SwapKeyUseCase
+    func makeGetUserIPInfoUseCase() -> GetUserIPInfoUseCase
 }
 
 class AuthAssembly: AuthAssembling {
+
     func makeSwapKeyUseCase() -> SwapKeyUseCase { // Dependency Inversion
         let client = SwapKeyClientImpl(url: SwapKeyEndpoint.swapKey)
         let repository = SwapKeyRepositoryImpl(client: client)
         return SwapKeyUseCaseImpl(repository: repository)
+    }
+    func makeGetUserIPInfoUseCase() -> any GetUserIPInfoUseCase {
+        let client = GetUserIPInfoClientImpl(url: GetUserIPInfoEndpoint.url)
+        let repository = GetUserIPInfoRepositoryImpl(client: client)
+        return GetUserIPInfoUseCaseImpl(repository: repository)
     }
 }
