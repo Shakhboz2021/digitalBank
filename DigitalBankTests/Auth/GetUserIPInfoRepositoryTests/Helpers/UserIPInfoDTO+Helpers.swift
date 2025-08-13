8187a2e02cac0f0e83e2b7972522559b703369df
Muhammad <muhammad.tohirov@fido.uz>
2025-08-13 11:38:47 +0500

    test(auth): add UserIPInfoDTO.mock helper for repository tests

---
+++ DigitalBankTests/Auth/GetUserIPInfoRepositoryTests/Helpers/UserIPInfoDTO+Helpers.swift
@@ -0,0 +1,48 @@
+//
+//  UserIPInfoDTO+Helpers.swift
+//  DigitalBankTests
+//
+//  Created by Muhammad on 13/08/25.
+//
+
+import Foundation
+
+@testable import DigitalBank
+
+extension UserIPInfoDTO {
+    static var mock: Self {
+        .init(
+            status: "success",
+            country: "Uzbekistan",
+            countryCode: "UZ",
+            region: "Toshkent",
+            regionName: "Toshkent",
+            city: "Tashkent",
+            zip: "100000",
+            lat: "41.3111",
+            lon: "69.2797",
+            timezone: "Asia/Tashkent",
+            isp: "Uztelecom",
+            org: "UZ",
+            as: "AS12345",
+            query: "198.51.100.10",
+            ip: "198.51.100.10"
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
