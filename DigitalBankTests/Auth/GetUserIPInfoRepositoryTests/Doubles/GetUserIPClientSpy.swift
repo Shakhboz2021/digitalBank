04478d7234f11f1fa3d1109eeac1451cd2a028e9
Muhammad <muhammad.tohirov@fido.uz>
2025-08-13 10:29:51 +0500

    test(auth): add GetUserIPInfoClientSpy for repository tests

---
+++ DigitalBankTests/Auth/GetUserIPInfoRepositoryTests/Doubles/GetUserIPClientSpy.swift
@@ -0,0 +1,28 @@
+//
+//  GetUserIPClientSpy.swift
+//  DigitalBankTests
+//
+//  Created by Muhammad on 13/08/25.
+//
+
+import Foundation
+@testable import DigitalBank
+
+class GetUserIPClientSpy: GetUserIPInfoClient {
+    private(set) var receivedRequests: [Void] = []
+    var resultToReturn: Result<UserIPInfoDTO, Error>?
+    
+    func fetchUserIPInfo() async throws -> UserIPInfoDTO {
+        receivedRequests.append(())
+        switch resultToReturn {
+            case .success(let userIPInfoDTO):
+                return userIPInfoDTO
+            case .failure(let error):
+                throw error
+            case .none:
+                fatalError("Set resultToReturn before calling fetchUserIPInfo()")
+        }
+    }
+
+    
+}
