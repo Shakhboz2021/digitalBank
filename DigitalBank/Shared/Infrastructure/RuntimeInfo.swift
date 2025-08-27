168f433590416faf690861d91478b4488dbed12b
Muhammad <muhammad.tohirov@fido.uz>
2025-08-27 11:20:39 +0500

    feat(runtime): toDTO heloer method and AppRuntimeDTO

--- DigitalBank/Shared/Infrastructure/RuntimeInfo.swift
+++ DigitalBank/Shared/Infrastructure/RuntimeInfo.swift
@@ -10,12 +10,9 @@ import UIKit
 enum RuntimeInfo {
     static func currect() -> AppRuntime {
         let os = UIDevice.current.systemVersion
-        let short =
-            Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
-            ?? "0"
-        let build =
-            Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "0"
-        let api = os.split(separator: ".").first.map(String.init) ?? os
+        let short = AppInfo.releaseVersion
+        let build = AppInfo.buildNumber
+        let api = "I"
         return .init(
             osVersion: os,
             appVersion: short,
