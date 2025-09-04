e1c12cc411dfb611dc576b879be100400245e936
Muhammad <muhammad.tohirov@fido.uz>
2025-09-04 11:20:27 +0500

    test(signin-endpoint): verify signin builds request to expected path

--- DigitalBankTests/Auth/SingIn/SignInEndpointTests/SignInEndpointTests.swift
+++ DigitalBankTests/Auth/SingIn/SignInEndpointTests/SignInEndpointTests.swift
@@ -11,12 +11,18 @@ import XCTest
 @testable import DigitalBank
 
 class SignInEndpointTests: XCTestCase {
+    // Arrange
+    let sut: Endpoint = AuthEndpoint.signIn
+
     func test_signIn_hasPostMethod() throws {
-        // Arrange
-        let sut: Endpoint = AuthEndpoint.signIn
         // Act
         let request = try sut.makeRequest()
         // Assert
         XCTAssertEqual(request.httpMethod, HTTPMethod.post.rawValue)
+    }
+    
+    func test_signIn_hasExpectedPath() throws {
+        let request = try sut.makeRequest()
+        XCTAssertEqual(request.url?.path, "/USER_SIGN_IN_NEW")
     }
 }
