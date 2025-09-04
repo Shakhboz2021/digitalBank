07ddca1344275a8a2b99147bb47a2d7ab8987c71
Muhammad <muhammad.tohirov@fido.uz>
2025-09-04 09:47:52 +0500

    feat(signin-clientImpl): add SignInClientImpl to work with network

---
+++ DigitalBank/Features/Auth/Data/Clients/SignIn/SignInClientImpl.swift
@@ -0,0 +1,37 @@
+//
+//  SignInClientImpl.swift
+//  DigitalBank
+//
+//  Created by Muhammad on 04/09/25.
+//
+
+import Foundation
+
+class SignInClientImpl: SignInClient {
+    private let network: NetworkSession
+    private let endpoint: Endpoint
+
+    init(network: NetworkSession, endpoint: Endpoint) {
+        self.network = network
+        self.endpoint = endpoint
+    }
+
+    func signIn(request: SignInDTO.Request) async throws -> SignInDTO.Response {
+
+        let urlRequest = try endpoint.makeRequest()
+
+        let (data, response) = try await network.data(for: urlRequest)
+
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkError.invalidResponse
+        }
+
+        guard (200...300).contains(httpResponse.statusCode) else {
+            throw NetworkError.invalidResponse
+        }
+
+        return try JSONDecoder().decode(SignInDTO.Response.self, from: data)
+
+    }
+
+}
